9e9552584303af5789465cb81160018725c13667
gwb2 <genthebestperson@gmail.com>
2026-03-05 22:01:11 -0800

    remove unnecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import PileCard from "@/components/PileCard/PileCard";
 import './styles/globals.scss'
 import "./styles/nim.scss";

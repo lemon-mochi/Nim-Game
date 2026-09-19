8af47cbfad4a62c29a81394838144138f4ab8f86
gwb2 <genthebestperson@gmail.com>
2026-09-19 13:32:21 -0700

    temporarily add print statement

--- backend/server.py
+++ backend/server.py
@@ -17,6 +17,8 @@ app = FastAPI()
 load_dotenv(dotenv_path=".env")
 origin = os.getenv("ORIGIN")
 
+print("ORIGIN =", origin)
+
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=False,
